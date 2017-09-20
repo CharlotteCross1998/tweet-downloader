72cbbb5544c243c594d00cb09e47646f82a6932c
Brad <bcross2@uclan.ac.uk>
2017-09-20 22:25:38 +0100

    allow up to 3600 tweets to be downloaded
    also improved I/O

--- script.py
+++ script.py
@@ -8,9 +8,18 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.user_timeline('kizuthemaid')
-for tweet in public_tweets:
-    print(tweet.text)
-    tweets = open("tweet.txt", 'a')
-    tweets.write(tweet.text)
-    tweets.write('\n')
+tweets_file = open("tweet.txt", 'a')
+tweets = ""
+for page in tweepy.Cursor(api.user_timeline,id='kizuthemaid', count=200).pages(16): #go through 16 pages of 200 tweets (3600 tweets)
+	    for status in page:
+            tweets += status.text + "\n"
+            """
+            optional extras:
+                you could also do some formatting here. 
+                    For example, ">" would display as "&gt;"
+                remove retweets by checking if the first 2 letters are "RT"
+                remove links via regex
+                remove @'s
+            """
+tweets_file.write("\n" + tweets)
+tweets_file.close()
